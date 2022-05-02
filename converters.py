3bcbfa56d9403ae1096e24c34756045d6087dbe1
Cihan Kaya <cihankaya@Cihans-MacBook-Pro.local>
2022-05-02 23:28:59 +0300

    init functions

---
+++ converters.py
@@ -0,0 +1,64 @@
+import base64
+from email.mime import base
+
+class Converters:
+    def __init__(self):
+        pass
+
+    def ascii_of_str(self, str_to_ascii):
+        code = ""
+        for my_char in str_to_ascii:
+            code += str(ord(my_char)) + " "
+        return code
+
+
+    def str_to_bytes(self, str_to_byte):
+        return str_to_byte.encode("ascii")
+    
+    def decimal_to_byte_array(self, dec_string):
+        return bytearray(dec_string)
+
+    def hex_to_byte_array(self, hexString):
+        return bytearray.fromhex(hexString)
+
+    def bytes_to_base64(self, bytes_to_base64):
+        return base64.b64encode(bytes_to_base64)
+
+    def base64BytesToString(self, base64bytes):
+        return base64bytes.decode("ascii")
+
+    def xor_cipher(self, plain_text, key):
+        if( len(plain_text) is not len(key)):
+            raise ValueError("text and key lengths are not the same")
+        else:
+            cipher_text = []
+            for (byte_text, byte_key) in zip(bytearray.fromhex(plain_text), bytearray.fromhex(key)):
+                cipher_text.append(byte_text^byte_key)
+        return bytearray(cipher_text)
+            
+    def pad_key(self, key, plain_text):
+        if isinstance(key, (bytes, bytearray)):
+            len_of_key = len(key.hex())
+            len_of_plain_text = len(plain_text.hex())
+            print(len_of_key)
+            print(len_of_plain_text)
+            if(len_of_key > len_of_plain_text):
+                #add zero to the plain text
+                len_difference = len_of_key - len_of_plain_text
+                pad_number = len_difference // 2
+                print(pad_number)
+                pad_str = key.hex()*pad_number
+                print(pad_str)
+            elif(len_of_key < len_of_plain_text):
+                len_difference = len_of_plain_text - len_of_key
+                pad_number = len_difference // 2
+                print(pad_number)
+                pad_str = key.hex()*pad_number
+                print(pad_str)
+            elif(len_of_key == len_of_plain_text):
+                len_difference = 0
+                print(len_difference)
+            else:
+                pass
+        else:
+            raise ValueError("bad format")
---
+++ main.py
@@ -0,0 +1,73 @@
+import base64
+from os import lseek
+import re
+from unittest import result
+from converters import Converters
+import string
+
+#exercise 1
+#hex to base64
+
+crypto_pal_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+converter = Converters()
+
+
+
+######question 1
+# bytearrayFromHex = converter.hex_to_byte_array(crypto_pal_string)
+# print("byte array from hex")
+# print(bytearrayFromHex)
+# print()
+# print("base64")
+# bytes_base64 = converter.bytes_to_base64(bytearrayFromHex)
+# string_bytes_base64 = converter.base64BytesToString(bytes_base64)
+# print(string_bytes_base64)
+# print(base64.b64encode(bytearrayFromHex).decode("ascii"))
+###############
+
+
+######question 2
+""" key = "686974207468652062756c6c277320657965"
+plain_text =         "1c0111001f010100061a024b53535009181c"
+bytearray_plain_text = bytearray.fromhex(plain_text)
+
+bytearray_key = bytearray.fromhex(key)
+
+
+
+cipher_text = converter.xor_cipher(key, plain_text)
+print(cipher_text.hex()) """
+
+########question 2######
+cipher_text = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+cipher_text_byte_array = bytearray.fromhex(cipher_text)
+len_of_cipher_text = len(cipher_text) // 2
+alphabet = list(string.ascii_letters)
+for char_alp in alphabet:
+    #print(char_alp)
+    key = char_alp
+    len_of_key = len(key.encode().hex()) // 2
+    if(len_of_key > len_of_cipher_text):
+        print("key is more than cipher text, pad cipher text")
+    elif(len_of_key == len_of_cipher_text):
+        print("no need padding")
+    elif len_of_key < len_of_cipher_text:
+        #print("pad key")
+        #print("difference between key and cipher text")
+        len_difference = len_of_cipher_text - len_of_key
+        #print("padding key ")
+        m_key = ""
+        for i in range(len_difference*2):
+            m_key += key.encode().hex()
+        m_key += key.encode().hex()
+        #print(m_key)
+        #print(len(m_key) // 2 - len_of_cipher_text)
+        #print()
+        #print()
+        result = bytes(a ^ b for(a, b) in zip(bytearray.fromhex(m_key), bytearray.fromhex(cipher_text)))
+        #print(result.decode())
+
+
+converter.pad_key("a".encode(), "asdfasd".encode())
+
+######question 3#######
